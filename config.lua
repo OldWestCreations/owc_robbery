442cc5ae3ed3bce17dc3684257ed2535d900809b
OldWestCreations <m.wollgramm90@gmail.com>
2025-03-05 14:35:22 -0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.SurrenderChance = 0.3 -- 30% -- find your own smooth-point
+
+Config.MinMoney = 0.01 -- min reward 
+Config.MaxMoney = 0.18 -- max reward
+
+Config.WeaponChance = 0.6 -- 60% -- find your own smooth-point
+
+Config.GiveMoneyAnimDict = 'script_rc@chrb@ig1_visit_clerk'
+Config.GiveMoneyAnim = 'arthur_gives_money_player'
+
+Config.Notification = { -- translations
+    AlreadyRobbed = {'Überfall Fehlgeschlagen', 'Du hast die Person bereits ausgeraubt!', 'generic_textures', 'cross', 4000},
+    MoneyDropped = {'Überfall erfolgreich', 'Beute: $%.2f', 'generic_textures', 'tick', 4000},
+    NPCFighting = {'Überfall fehlgeschlagen', 'Die Person kooperiert nicht!', 'generic_textures', 'cross', 4000}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "adamant"
+lua54 'yes'
+games {'rdr3'}
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'HerrScaletta | Old West Creations'
+description 'NPC Surrender and Money Drop Script'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
